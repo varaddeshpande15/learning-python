a43b9f68dcb064a6b37cc627f30a2dfd960327db
Varad Deshpande <111638914+varaddeshpande15@users.noreply.github.com>
2023-01-01 07:31:05 +0530

    Variables and type in Python

---
+++ 01_variables.py
@@ -0,0 +1,14 @@
+a="harry" #String
+b = 345 #Integer
+c = 45.32 #Float
+d='''harry'''
+e=None
+f=True
+print(a,b,c,d,e,f)
+#Printing type of variable -
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
